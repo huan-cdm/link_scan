5b7710291551397b5d0a2707ef7f72c7450b931f
huan-cdm <huan151009@163.com>
2023-09-19 10:33:06 +0800

    删除测试数据

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 black_list = ['text/html','text/javascript','application/javascript',
 'text/ecmascript','application/ecmascript','text/plain','packages',
 'M/D/yy','.gif','.vue','jpeg','jpg','png','apng','avif','text/xml',
-'application/x-www-form-urlencoded','.css','yyyy/mm/dd']
+'application/x-www-form-urlencoded','.css','yyyy/mm/dd','Usage:','Error:']
